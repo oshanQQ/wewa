964b806c3873cda684e106c2d14f558fd269e79b
oshanQQ <oshanqq@gmail.com>
2022-08-16 00:16:23 +0900

    ✨ Reseive a files path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(name = "wewa", author = "author: oshanQQ", about = "Command line tool to convert from webm to wav")]
+struct Arguments {
+    #[clap(value_parser)]
+    file_name: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::parse();
+
+    if let Some(file_name) = arguments.file_name.as_deref() {
+        print!("File name is \"{}\"\n", file_name);
+    }
 }
